9eacb91f32c57f64d3d0f9c46de995c906c8007f
Pranya15 <patelpranya15@gmail.com>
2025-11-07 13:10:27 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,13 @@
+class largest{
+    public static void main(String[] args){
+        String str="hello is powerfull";
+        String a[]=str.split(" ");
+        String largest="";
+        for(String val:a){
+            if(val.length()>largest.length()){
+                largest=val;
+            }
+        }
+        System.out.println(largest);
+    }
+}
