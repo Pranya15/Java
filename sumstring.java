efabd380418a94c051e2b92c764119d68ed36627
Pranya15 <patelpranya15@gmail.com>
2025-11-11 15:16:08 +0530

    Add files via upload

---
+++ sumstring.java
@@ -0,0 +1,9 @@
+public class sumstring {
+    static void sum(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args){
+        sumstring obj=new sumstring();
+        obj.sum();
+    }
+}
