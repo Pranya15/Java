d79e35db3be7cf88398ca44fbe9240399873001f
Pranya15 <patelpranya15@gmail.com>
2025-11-11 15:33:48 +0530

    Add files via upload

---
+++ staticclass.java
@@ -0,0 +1,13 @@
+public class staticclass{
+    static String name="Pranyaa!!!";
+    void Sum(){
+        System.out.println(name);
+    }
+}
+class main{
+    public static void main(String[] args) {
+        staticclass obj= new staticclass();
+        obj.Sum();
+    }
+}
+
