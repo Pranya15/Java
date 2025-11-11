6a89c4c165bd3895b35ec0b0c24ad2e3968f1af7
Pranya15 <patelpranya15@gmail.com>
2025-11-11 15:15:49 +0530

    Add files via upload

---
+++ immutable.java
@@ -0,0 +1,11 @@
+public class immutable {
+    public static void main(String[] args) {
+        String str="Hello";
+        String str1="H";
+        for(char ch:str.toCharArray()){
+            str1+=ch;
+            System.out.println(str1);
+
+        }
+    }
+} //IMMUTABLE
