c14b976eef61f50e528b3557f7ea9f02fa32b853
Pranya15 <patelpranya15@gmail.com>
2025-11-28 20:40:10 +0530

    Add files via upload

---
+++ linkedhashset.java
@@ -0,0 +1,11 @@
+import java.util.LinkedHashSet;
+
+class linkedhashset {
+    public static void main(String[] args){
+        LinkedHashSet<Integer> set=new LinkedHashSet<>();
+        set.add(7);
+        set.add(6);
+        set.add(8);
+        System.out.println(set);
+    }
+}
