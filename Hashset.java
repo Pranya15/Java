cf3f13cf5da734c307094b43f0fd5ff28ea851a0
Pranya15 <patelpranya15@gmail.com>
2025-11-28 20:40:58 +0530

    Add files via upload

---
+++ Hashset.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+ class Hashset {
+    public static void main(String[] args){
+        HashSet<Integer> set=new HashSet();
+        set.add(5);
+        set.add(6);
+        set.add(10);
+        set.contains(10);
+        System.out.println(set);
+    }
+}
