7271605914ae514b1d0838cdbac29b88a64803f3
Pranya15 <patelpranya15@gmail.com>
2025-11-28 20:39:33 +0530

    Add files via upload

---
+++ treeset.java
@@ -0,0 +1,9 @@
+ import java.util.TreeSet;
+ class treeset {
+     public static void main(String[] args){
+         TreeSet<Integer> set=new TreeSet<>();
+         set.add(5);
+         set.add(3);
+         System.out.println(set);
+     }
+}
